8ea8446c9e4320c25d2c46f0420f02deb63ce410
SomeDebris <mags2ramsey@gmail.com>
2022-03-07 10:48:57 -0800

    Add first scale of long right tri to shapes file.
    still doesnt work!!

--- shapes.lua
+++ shapes.lua
@@ -1,11 +1,39 @@
 -- RPS SHAPES FILE.
 -- written by Debris
+-------START------
 
---{
-    --{
-        --430,
-        --{
-            --{
-                --verts = {
-                    --{x,y},
-                    --etc
+-- LONG-TRI WITH DIFFERENT MIRRORING STRATEGY
+-- consider making it a braidedheadman tri?
+{
+    {
+        430,
+        {
+            {
+                verts = {
+                    {00.00, 10.00},
+                    {20.00, 00.00},
+                    {00.00, 00.00},
+                },
+                ports = {
+                    {0, 0.27637},
+                    {0, 0.72358},
+
+                    {1, 0.25000},
+                    {1, 0.75000},
+
+                    {2, 0.50000},
+                }
+            }
+            {
+                verts = {
+                    {00.00, 20.00},
+                    {40.00, 00.00},
+                    {00.00, 00.00},
+                },
+                ports = {
+                    {0, 
+        }
+    }
+    {432, mirror_of=430, {}}    -- mirror of LONG TRI copy
+}
+
