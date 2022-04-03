9fb8252092b33a5c3a50931d1561ef1f950054d3
SomeDebris <mags2ramsey@gmail.com>
2022-04-03 15:23:12 -0700

    Complete PAPER Faction Default Block
    Didn't select colors. THIS IS IMPORTANT.

--- faction-blocks/44_PAPER.lua
+++ faction-blocks/44_PAPER.lua
@@ -2,6 +2,21 @@
 
 {   -- PAPER Faction Default Block. DO NOT USE ON SHIPS
     44000,
+    group       = 44,
     name        = "DEFAULT PAPER BLOCK",
-    blurb       = "Block ID: 44000\nfeatures=NOPALETTE\n\n
+    blurb       = "Block ID: 44000\nfeatures=NOPALETTE\nA DEFAULT PAPER FACTION BLOCK TEMPLATE NOT INTENDED FOR USE ON SHIPS."
+    features    = NOPALETTE,
+    shape       = SQUARE,
+    scale       = 1,
+    points      = -1,
 
+    durability  = .5,
+    density     = .25,
+    growRate    = 50,
+    
+    -- TODO: find nice colors for PAPER Faction
+
+    --fillColor   = 0x404040,
+    --fillColor1  = 0x505050,
+    --lineColor   = 0x101010,
+},
