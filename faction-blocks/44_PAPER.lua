098892e58cc61337f0e83d710dfbab1fd92cbca2
SomeDebris <mags2ramsey@gmail.com>
2022-04-03 15:14:35 -0700

    add basics of Paper faction blocks.
    INCOMPLETE! WILL NOT RUN WITH REASSEMBLY.

---
+++ faction-blocks/44_PAPER.lua
@@ -0,0 +1,7 @@
+-- The Paper faction's Blocks file.
+
+{   -- PAPER Faction Default Block. DO NOT USE ON SHIPS
+    44000,
+    name        = "DEFAULT PAPER BLOCK",
+    blurb       = "Block ID: 44000\nfeatures=NOPALETTE\n\n
+
